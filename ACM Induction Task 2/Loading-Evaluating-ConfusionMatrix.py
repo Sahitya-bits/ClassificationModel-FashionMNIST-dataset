65dc53a5f34d6ae2dc740d9cd2eb071c46880dde
Sahitya-bits <rjsahitya007@gmail.com>
2025-06-11 23:48:09 +0530

    Add files via upload

---
+++ ACM Induction Task 2/Loading-Evaluating-ConfusionMatrix.py
@@ -0,0 +1,121 @@
+import torch
+from torch import nn
+
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+import numpy
+
+from tqdm.auto import tqdm
+from torchmetrics import ConfusionMatrix
+from mlxtend.plotting import plot_confusion_matrix
+
+train_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=False,
+    transform=ToTensor(),
+    target_transform=None
+)
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=False,
+    transform=ToTensor(),
+    target_transform=None
+)
+
+BATCH_SIZE = 32 
+train_dataloader = DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
+test_dataloader = DataLoader(dataset=test_data, batch_size=BATCH_SIZE, shuffle=False)
+class_names = train_data.classes
+
+class CNN_Model(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units*7*7,
+                      out_features=output_shape)
+        )
+    def forward(self,x):
+        x = self.classifier(self.conv_block_2(self.conv_block_1(x)))
+        return x
+
+### Loading our CNN-Model :-
+model = CNN_Model(input_shape=1, hidden_units=10, output_shape=len(class_names))
+model.load_state_dict(torch.load(f="models\CNN-Model.pt"))
+
+### Setting up loss_fn & accuracy_fn for evaluating our model through test_set :-
+loss_fn = torch.nn.CrossEntropyLoss()
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct / len(y_pred)) * 100
+    return acc
+
+### Evaluating our model over the test_set :-
+def test_step(model: torch.nn.Module,
+              data_loader: torch.utils.data.DataLoader,
+              loss_fn: torch.nn.Module,
+              accuracy_fn):
+    test_loss, test_acc = 0, 0
+    model.eval()
+    with torch.inference_mode():
+        for x,y in data_loader:
+            test_pred = model(x)
+            test_loss += loss_fn(test_pred, y)
+            test_acc += accuracy_fn(y_true=y, y_pred=test_pred.argmax(dim=1))
+
+        test_loss /= len(data_loader)
+        test_acc /= len(data_loader)
+    print(f"Test-loss: {test_loss:.4f} | Test-acc: {test_acc:.2f}%")
+
+test_step(model, test_dataloader, loss_fn, accuracy_fn)
+
+### Displaying Confusion matrix :-
+y_preds = []
+model.eval()
+
+with torch.inference_mode():
+    for x,y in tqdm(test_dataloader, desc="Making predictions..."):
+        y_logit = model(x)
+        y_pred = torch.softmax(y_logit.squeeze(), dim=0).argmax(dim=1)
+        y_preds.append(y_pred)
+y_pred_tensor = torch.cat(y_preds)
+
+confmat = ConfusionMatrix(num_classes=len(class_names), task="multiclass")
+confmat_tensor = confmat(preds=y_pred_tensor, target=test_data.targets)
+fig, ax = plot_confusion_matrix(conf_mat=confmat_tensor.numpy(), class_names=class_names, figsize=(10,7))
+plt.show()
